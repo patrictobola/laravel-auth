766f6e59d29521488ef18372b2c8c92e5a728c0d
patrictobola <tobola92@gmail.com>
2023-08-31 16:18:53 +0200

    done migration and seed

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
+use Faker\Generator as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,8 +12,13 @@ class ProjectSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        $project = new Project();
+        $project->title = $faker->words(3, true);
+        $project->description = $faker->text();
+        $project->date = $faker->date();
+        $project->thumb = $faker->imageUrl(200, 200);
+        $project->save();
     }
 }
