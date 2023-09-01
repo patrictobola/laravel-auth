04ddc7e07cd1ff7c5bc505d5817406dcbf8572b3
patrictobola <tobola92@gmail.com>
2023-09-01 16:48:30 +0200

    done css validation in page create

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -20,19 +20,44 @@
             @csrf
             <div class="mb-3">
                 <label for="title" class="form-label">Title</label>
-                <input type="text" class="form-control" id="title" name="title">
+                <input type="text" class="form-control @error('title') is-invalid @enderror" id="title"
+                    name="title">
+                @error('title')
+                    <div class="invalid-feedback">
+                        Please provide a valid title.
+                    </div>
+                @enderror
+
             </div>
             <div class="mb-3">
                 <label for="description" class="form-label">Description</label>
-                <textarea type="text" class="form-control" id="description" name="description" rows="4"></textarea>
+                <textarea type="text" class="form-control @error('description') is-invalid @enderror" id="description"
+                    name="description" rows="4"></textarea>
+                @error('description')
+                    <div class="invalid-feedback">
+                        Please provide a valid description.
+                    </div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="date" class="form-label">Date</label>
-                <input type="date" class="form-control" id="date" name="date" value="">
+                <input type="date" class="form-control @error('date') is-invalid @enderror" id="date" name="date"
+                    value="">
+                @error('date')
+                    <div class="invalid-feedback">
+                        Please provide a valid date.
+                    </div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="thumb" class="form-label">Screenshot</label>
-                <input type="text" class="form-control" id="thumb" name="thumb" value="">
+                <input type="text" class="form-control @error('thumb') is-invalid @enderror" id="thumb"
+                    name="thumb" value="">
+                @error('thumb')
+                    <div class="invalid-feedback">
+                        Please provide a valid screenshot URL.
+                    </div>
+                @enderror
             </div>
 
             <div class="d-flex justify-content-between mt-4">
